e885e80f8779783af3a1a910fe8e2fc384614c56
ellypereira <ellysouza960@gmail.com>
2026-08-12 18:21:12 -0300

    att

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -12,27 +12,15 @@ var move_direction: Vector2 = Vector2.ZERO
 
 
 func _physics_process(_delta: float) -> void:
-	read_input()
-	move_character()
-
-
-# ============================================================
-# ENTRADA
-# ============================================================
-
-func read_input() -> void:
 	move_direction = Input.get_vector(
 		"move_left",
 		"move_right",
 		"move_up",
 		"move_down"
 	)
-
-
-# ============================================================
-# MOVIMENTO
-# ============================================================
-
-func move_character() -> void:
+	
 	velocity = move_direction * move_speed
 	move_and_slide()
+	
+	if move_direction != Vector2.ZERO:
+		print("PLAYER", global_position, "VELOCITY: ", velocity)
